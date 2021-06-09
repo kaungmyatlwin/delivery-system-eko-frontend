9222d303bccf87dd564ed8686666cb6c3ab20556
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2021-06-09 16:38:16 +0630

    feat: CaseOne component refinery

--- src/components/CaseOne.js
+++ src/components/CaseOne.js
@@ -2,16 +2,31 @@ import { useState } from 'react';
 import {
   Button,
   Select,
-  MenuItem
+  MenuItem,
+  Grid,
+  InputLabel,
+  Typography,
 } from '@material-ui/core';
 import { getTowns } from '../helpers/utils';
+import { getTotalCostOfRoute } from '../helpers/routesLogic';
 
 const styles = {
+  addTownButton: {
+    float: 'right',
+  },
   selectTownsStyle: {
     width: '100%',
     marginTop: 5,
     marginBottom: 5,
   },
+  calculateButtonGrid: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  totalCost: {
+    marginTop: 10,
+    textAlign: 'center',
+  }
 };
 
 const towns = getTowns();
@@ -19,6 +34,8 @@ const towns = getTowns();
 export default function CaseOne() {
   const [routedTowns, setRoutedTowns] = useState([]);
   const [totalCost, setTotalCost] = useState(null);
+
+  const disableCalculateButton = routedTowns.length === 0 || routedTowns.includes(null);
 
   function onClickAddTown() {
     const clone = [...routedTowns];
@@ -32,25 +49,56 @@ export default function CaseOne() {
     setRoutedTowns(clone);
   }
 
+  function onClickCalculateCost() {
+    setTotalCost(getTotalCostOfRoute(routedTowns));
+  }
+
   return (
     <>
-      <div>
+      <Grid
+        container
+        justify="flex-end"
+      >
+        <Button
+          disableElevation
+          color="primary"
+          onClick={onClickAddTown}>
+            Add Town
+        </Button>
+      </Grid>
+      <div style={{ textAlign: 'left' }}>
         {routedTowns.map((routedTown, index) => (
-          <Select
-            style={styles.selectTownsStyle}
-            key={index}
-            label={`Town ${index + 1}`}
-            onChange={(e) => onChangeSelectTown(e, index)}
-          >
-          {
-            towns.map((town) => (
-                <MenuItem key={town} value={town}>{town}</MenuItem>
-              ))
-            }
-          </Select>
+          <div>
+            <InputLabel id={`town${index}`} shrink>Town {index + 1}</InputLabel>
+            <Select
+              id={`town${index}`}
+              placeholder="Town 1"
+              style={styles.selectTownsStyle}
+              key={index}
+              onChange={(e) => onChangeSelectTown(e, index)}
+            >
+            {
+              towns.map((town) => (
+                  <MenuItem key={town} value={town}>{town}</MenuItem>
+                ))
+              }
+            </Select>
+          </div>
         ))}
       </div>
-      <Button color="primary" onClick={onClickAddTown}>Add Town</Button>
+      <div style={styles.calculateButtonGrid}>
+        <Button
+          disabled={disableCalculateButton}
+          variant="contained"
+          color="primary"
+          disableElevation
+          onClick={onClickCalculateCost}>Calculate Cost</Button>
+      </div>
+      <div style={styles.totalCost}>
+        <Typography variant="h6">
+          {totalCost}
+        </Typography>
+      </div>
     </>
   );
 }
