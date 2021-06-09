0c469124a6882e862fa09a75e4e734fd9e3dc474
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2021-06-10 02:07:55 +0630

    feat: implement recursive route finding

--- src/helpers/routesLogic.js
+++ src/helpers/routesLogic.js
@@ -1,5 +1,10 @@
 import { routes } from './constants';
 
+/**
+ * Gets the total costs of route between each towns.
+ * @param {array} towns
+ * @returns {string|number}
+ */
 export function getTotalCostOfRoute(towns = []) {
   if (!towns || towns.length === 1) {
     return 'Add more towns to build a route.';
@@ -25,4 +30,45 @@ export function getTotalCostOfRoute(towns = []) {
   }
 
   return costs.reduce((total, currentCost) => total + currentCost, 0);
+}
+
+/**
+ * Gets all the possible deliver routes between 2 towns.
+ * @param {Number} t1 Town 1
+ * @param {Number} t2 Town 2
+ * @returns {Number}
+ */
+export function getPossibleDeliveryRoutes(t1, t2, maxStops, useSameRoute = false) {
+  if (!t1 || !t2) return;
+
+  let totalPossibleRoutes = 0;
+  const startPoint = routes[t1];
+  const allPoints = {};
+
+  Object.entries(routes).forEach(([key, value]) => {
+    allPoints[key] = Object.keys(value)
+  });
+
+  // Lookup Table
+  const startNodes = Object.keys(startPoint);
+  traverseNodes(startNodes);
+
+  function traverseNodes(nodes) {
+    if (!nodes || nodes.length === 0) {
+      return;
+    };
+
+    if (nodes.includes(t1)) return;
+
+    for (let i = 0; i < nodes.length; i++) {
+      const innerNodes = allPoints[nodes[i]]; // 0 -> B, C, D, B -> E, C -> D, F
+      const foundRoute = innerNodes.findIndex((n) => n === t2);
+      if (foundRoute > -1) {
+        totalPossibleRoutes += 1;
+      }
+      traverseNodes(innerNodes);
+    }
+  }
+
+  return totalPossibleRoutes;
 }
