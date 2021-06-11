02b2c6f8fc9ef6250c4199714a6361350e54bab4
Kaung Myat Lwin <dev.kaungmyatlwin@gmail.com>
2021-06-11 22:41:58 +0630

    chore: fix test case meaning

--- src/tests/case2.test.js
+++ src/tests/case2.test.js
@@ -1,7 +1,7 @@
 import { getPossibleDeliveryRoutes } from '../helpers/routesLogic';
 
 describe('Case 2', () => {
-  it('E-D delivery maximum possible routes should return 4', () => {
+  it('E-D delivery maximum possible routes with 4 stops should return 4', () => {
     expect(getPossibleDeliveryRoutes('E', 'D', 4)).toBe(4);
   });
 
